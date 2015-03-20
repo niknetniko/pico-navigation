c34de287ae352fb337d42b640288d5c80cbfd66a
Niko Strijbol <strijbol.niko@gmail.com>
2015-03-20 20:04:44 +0100

    Update doc and readme

--- README.md
+++ README.md
@@ -1,61 +1,95 @@
 # Pico Navigation Plugin
 
-This is a Plugin for the Flat File Based CMS named [Pico](pico.dev7studios.com/).
+This is a plugin for the Flat File Based CMS named [Pico](pico.dev7studios.com/).
 
-## use
+This is a fork of Ahmet Topal's [plugin](https://github.com/ahmet2106/pico-navigation). It provides these additional functions:
 
-Copy the `at_navigation.php` File to the `plugins` Folder in the Root of your Pico Project. Every Plugin in this Folder is activated automatically.
+* Manual page sorting. You can specify in what order pages are displayed.
+* Exclude pages with regex functions. Thanks to [oliverlorenz](https://github.com/oliverlorenz).
 
-In your **theme** you only have to add this line where your navigation should be:
+## Usage
 
+### Installation & Basics
+
+To install the plugin, follow these steps:
+
+1. Go to [releases](https://github.com/niknetniko/pico-navigation/releases) and download the latest version.
+2. Copy downloaded file (`at_navigation.php`) to the `plugins` folder in the root of your Pico project.
+3. Add the following line in your **theme** where your navigation should be:
+
+    ```
     {{ at_navigation.navigation }}
+    ```
 
-I redesigned the style of the **default theme** for a quick preview. The output is like this:
+    Alternatively, you can use an adapted version of the default theme (included in the download). This looks like this:
 
-![Default Theme with at_navigation Plugin](img.png)
+	![Default Theme with at_navigation Plugin](img.png)
 
-just replace the `default` Theme Folder with the one in this repo and add this line to your config.php
+	Just replace the `default` theme folder with the one in the download and add this line to your config.php
 
+	```
     $config['at_navigation']['class'] = 'nav';
+    ```
 
 
-### set id and class of navigation element
+### Options
 
-Add these two lines to your `config.php` to change **id** and/ or **class** from `at-navigation` to anything you want:
+You can customize the plugin with a bunch of options. To use them, simply add them to your `config.php` file.
+
+#### Set the id and class of the navigation
+
+Set the **id** and/or **class** from `at-navigation` to anything you want:
 
 ```
 $config['at_navigation']['id'] = 'at-navigation';
 $config['at_navigation']['class'] = 'at-navigation';
 ```
 
-### set class of list items and links
+#### Set class of list items and links
+
+Set the **id** and/or **class** from the list items to anything you want:
 
 ```
 $config['at_navigation']['class_li'] = 'list-item';
 $config['at_navigation']['class_a'] = 'link-item';
 ```
 
-### exclude pages and folders
-Add these two lines to your `config.php` to exclude **single pages** and/ or **folders**:
+### Exclude pages and folders
+Exclude **single pages** and/or **folders**:
 
 ```
 $config['at_navigation']['exclude']['single'] = array('a/site', 'another/site');
 $config['at_navigation']['exclude']['folder'] = array('a/folder', 'another/folder');
 ```
 
-### exclude via regular expression
-Add these line to your `config.php` to exclude all blog posts from menu:
+You can also exclude pages based on **regular expressions**. For example, you can exclude all blog posts:
 
 ```
 $config['at_navigation']['exclude']['regex'] = array('/^blog\/[\w-]+\/*$/');
 ```
 
-## what it does
+### Order pages
+To set the order of the pages manually, you must add a meta tag to the page header:
 
-This Plugin generates a better navigation with child navigations and editable configuration.
+```
+Order: 1
+```
+
+For child menu's, the `Order` tag from the `index.md` file in the subfolder will be used. The other pages in the child menu have their own order, so you can start counting at 0 again.
+
+A few remarks about the sorting:
+
+* Pages without `Order` tag will be sorted alphabetically.
+* Pages with a tag will be sorted before pages without tag.
+* Pages with the same `Order` tag will not be sorted.
+
+## What it does
+
+This plugin generates a better navigation with child menu's and editable configuration.
 
 So the output looks like:
 
+	```
     <ul id="at-navigation" class="at-navigation">
         <li><a href="…" title="…">…</a></li>
         <li>
@@ -65,17 +99,20 @@ So the output looks like:
             </ul>
         </li>
     </ul>
+    ```
 
 As you can see it will add an `.is-active` class to the `<a>` and `<li>` element of the **active page**.
 
 
-## licence
+## Licence
 
 CreativeCommons2.0 licence: [CC BY-SA](http://creativecommons.org/licenses/by-sa/2.0/)
 
 You are free to share & remix this code only if you mention me as coder of this base.
 
 
-## copyright
+## Credits
 
-**Copyright © Ahmet Topal 2013. All rights reserved.**
+Original plugin: Ahmet Topal
+Sorting functions: Niko Strijbol
+Regex excluding: Oliver Lorenz
--- at_navigation.php
+++ at_navigation.php
@@ -1,34 +1,54 @@
 <?php
 
 /**
- * navigation plugin which generates a better configurable navigation with endless children navigations
+ * Navigation plugin to generate a better, more configurable navigation with (theoretically endless) subpages.
  *
  * @author Ahmet Topal
+ * @author Niko Strijbol <strijbol.niko@gmail.com>
+ * @author Oliver Lorenz
+ *
  * @link http://ahmet-topal.com
+ *
  * @license http://opensource.org/licenses/MIT
  */
 class AT_Navigation
 {
-    ##
-    # VARS
-    ##
+    /**
+     * Holds the settings.
+     */
     private $settings = array();
-    private $navigation = array();
-
-    ##
-    # HOOKS
-    ##
 
     /**
-     * Add the meta tag Order
+     * Holds the actual navigation.
+     */
+    private $navigation = array();
+
+    /**
+     * Add the meta tag Order. With this tag, it is possible to manually specify the order in which the menu pages
+     * are displayed in the navigation. Each child menu (a folder in the file system) starts to count again. The index
+     * file will be used to determine the order of the dropdown menu.
+     *
+     * For example:
+     *      start.md - Order: 1
+     *      folder/index.md - Order: 2
+     *      folder/subpage.md - Order: 1
+     *      test.md - Order: 3
+     * will output this menu:
+     *      start
+     *      index
+     *      |- subppage
+     *      test
+     *
+     * @see sort_pages for the exact logic.
+     *
      * @author Niko Strijbol
      */
-    public function before_read_file_meta(&$headers)
+    public function before_read_file_meta( &$headers )
     {
         $headers['order'] = "Order";
     }
 
-    public function get_page_data(&$data, $page_meta)
+    public function get_page_data( &$data, $page_meta )
     {
         //Loads all the meta values, including meta values from headers into page values
         foreach ($page_meta as $key => $value) {
@@ -36,82 +56,79 @@ class AT_Navigation
         }
     }
 
-    public function get_pages(&$pages, &$current_page, &$prev_page, &$next_page)
+    public function get_pages( &$pages, &$current_page, &$prev_page, &$next_page )
     {
         global $config;
         $navigation = array();
 
         foreach ($pages as $page) {
-            if (!$this->at_exclude($page)) {
-                $_split = explode('/', substr($page['url'], strlen($this->settings['base_url']) + 1));
-                $navigation = array_merge_recursive($navigation, $this->at_recursive($_split, $page, $current_page));
+            if ( ! $this->at_exclude( $page )) {
+                $_split     = explode( '/', substr( $page['url'], strlen( $this->settings['base_url'] ) + 1 ) );
+                $navigation = array_merge_recursive( $navigation,
+                    $this->at_recursive( $_split, $page, $current_page ) );
             }
         }
 
-        array_multisort($navigation);
+        array_multisort( $navigation );
 
         $this->navigation = $navigation;
     }
 
-    public function config_loaded(&$settings)
+    public function config_loaded( &$settings )
     {
         $this->settings = $settings;
 
         // default id
-        if (!isset($this->settings['at_navigation']['id'])) {
+        if ( ! isset( $this->settings['at_navigation']['id'] )) {
             $this->settings['at_navigation']['id'] = 'at-navigation';
         }
 
         // default classes
-        if (!isset($this->settings['at_navigation']['class'])) {
+        if ( ! isset( $this->settings['at_navigation']['class'] )) {
             $this->settings['at_navigation']['class'] = 'at-navigation';
         }
-        if (!isset($this->settings['at_navigation']['class_li'])) {
+        if ( ! isset( $this->settings['at_navigation']['class_li'] )) {
             $this->settings['at_navigation']['class_li'] = 'li-item';
         }
-        if (!isset($this->settings['at_navigation']['class_a'])) {
+        if ( ! isset( $this->settings['at_navigation']['class_a'] )) {
             $this->settings['at_navigation']['class_a'] = 'a-item';
         }
 
         // default excludes
         $this->settings['at_navigation']['exclude'] = array_merge_recursive(
-            array('single' => array(), 'folder' => array(), 'regex' => array()),
-            isset($this->settings['at_navigation']['exclude']) ? $this->settings['at_navigation']['exclude'] : array()
+            array( 'single' => array(), 'folder' => array(), 'regex' => array() ),
+            isset( $this->settings['at_navigation']['exclude'] ) ? $this->settings['at_navigation']['exclude'] : array()
         );
     }
 
-    public function before_render(&$twig_vars, &$twig)
+    public function before_render( &$twig_vars, &$twig )
     {
-        $twig_vars['at_navigation']['navigation'] = $this->at_build_navigation($this->navigation, true);
+        $twig_vars['at_navigation']['navigation'] = $this->at_build_navigation( $this->navigation, true );
     }
 
-    ##
-    # HELPER
-    ##
-
-    private function at_build_navigation($navigation = array(), $start = false)
+    private function at_build_navigation( $navigation = array(), $start = false )
     {
-        $id = $start ? $this->settings['at_navigation']['id'] : '';
-        $class = $start ? $this->settings['at_navigation']['class'] : '';
+        $id       = $start ? $this->settings['at_navigation']['id'] : '';
+        $class    = $start ? $this->settings['at_navigation']['class'] : '';
         $class_li = $this->settings['at_navigation']['class_li'];
-        $class_a = $this->settings['at_navigation']['class_a'];
-        $child = '';
-        $ul = $start ? '<ul id="%s" class="%s">%s</ul>' : '<ul>%s</ul>';
+        $class_a  = $this->settings['at_navigation']['class_a'];
+        $child    = '';
+        $ul       = $start ? '<ul id="%s" class="%s">%s</ul>' : '<ul>%s</ul>';
 
-        if (isset($navigation['_child'])) {
+        if (isset( $navigation['_child'] )) {
             $_child = $navigation['_child'];
 
             //Sort the pages.
-            uasort($_child, array($this, "sort_pages"));
+            uasort( $_child, array( $this, "sort_pages" ) );
 
             foreach ($_child as $c) {
-                $child .= $this->at_build_navigation($c);
+                $child .= $this->at_build_navigation( $c );
             }
 
-            $child = $start ? sprintf($ul, $id, $class, $child) : sprintf($ul, $child);
+            $child = $start ? sprintf( $ul, $id, $class, $child ) : sprintf( $ul, $child );
         }
 
-        $li = isset($navigation['title'])
+        $li = isset( $navigation['title'] )
             ? sprintf(
                 '<li class="%1$s %5$s"><a href="%2$s" class="%1$s %6$s" title="%3$s">%3$s</a>%4$s</li>',
                 $navigation['class'],
@@ -126,15 +143,15 @@ class AT_Navigation
         return $li;
     }
 
-    private function at_exclude($page)
+    private function at_exclude( $page )
     {
         $exclude = $this->settings['at_navigation']['exclude'];
-        $url = substr($page['url'], strlen($this->settings['base_url']) + 1);
-        $url = (substr($url, -1) == '/') ? $url : $url . '/';
+        $url     = substr( $page['url'], strlen( $this->settings['base_url'] ) + 1 );
+        $url     = ( substr( $url, - 1 ) == '/' ) ? $url : $url . '/';
 
         foreach ($exclude['single'] as $s) {
-            $s = (substr($s, -1 * strlen('index')) == 'index') ? substr($s, 0, -1 * strlen('index')) : $s;
-            $s = (substr($s, -1) == '/') ? $s : $s . '/';
+            $s = ( substr( $s, - 1 * strlen( 'index' ) ) == 'index' ) ? substr( $s, 0, - 1 * strlen( 'index' ) ) : $s;
+            $s = ( substr( $s, - 1 ) == '/' ) ? $s : $s . '/';
 
             if ($url == $s) {
                 return true;
@@ -142,17 +159,16 @@ class AT_Navigation
         }
 
         foreach ($exclude['folder'] as $f) {
-            $f = (substr($f, -1) == '/') ? $f : $f . '/';
-            $is_index = ($f == '' || $f == '/') ? true : false;
+            $f        = ( substr( $f, - 1 ) == '/' ) ? $f : $f . '/';
+            $is_index = ( $f == '' || $f == '/' ) ? true : false;
 
-            if (substr($url, 0, strlen($f)) == $f || $is_index) {
+            if (substr( $url, 0, strlen( $f ) ) == $f || $is_index) {
                 return true;
             }
         }
 
-        foreach ($exclude['regex'] as $r)
-        {
-            if (preg_match($r, $url)) {
+        foreach ($exclude['regex'] as $r) {
+            if (preg_match( $r, $url )) {
                 return true;
             }
         }
@@ -160,47 +176,57 @@ class AT_Navigation
         return false;
     }
 
-    private function at_recursive($split = array(), $page = array(), $current_page = array())
+    private function at_recursive( $split = array(), $page = array(), $current_page = array() )
     {
-        $activeClass = (isset($this->settings['at_navigation']['activeClass'])) ? $this->settings['at_navigation']['activeClass'] : 'is-active';
-        if (count($split) == 1) {
+        $activeClass = ( isset( $this->settings['at_navigation']['activeClass'] ) ) ? $this->settings['at_navigation']['activeClass'] : 'is-active';
+        if (count( $split ) == 1) {
             $ret = array(
                 'title' => $page['title'],
-                'url' => $page['url'],
+                'url'   => $page['url'],
                 'order' => $page['order'],
-                'class' => ($page['url'] == $current_page['url']) ? $activeClass : ''
+                'class' => ( $page['url'] == $current_page['url'] ) ? $activeClass : ''
             );
 
-            $split0 = ($split[0] == '') ? '_index' : $split[0];
-            return array('_child' => array($split0 => $ret));
+            $split0 = ( $split[0] == '' ) ? '_index' : $split[0];
+            return array( '_child' => array( $split0 => $ret ) );
         } else {
             if ($split[1] == '') {
-                array_pop($split);
-                return $this->at_recursive($split, $page, $current_page);
+                array_pop( $split );
+                return $this->at_recursive( $split, $page, $current_page );
             }
 
-            $first = array_shift($split);
-            return array('_child' => array($first => $this->at_recursive($split, $page, $current_page)));
+            $first = array_shift( $split );
+            return array( '_child' => array( $first => $this->at_recursive( $split, $page, $current_page ) ) );
         }
     }
 
     /**
-     * Sort the pages according to the meta tag Order
+     * Compare two pages.
+     *
+     * If the meta tag Order is not set, the pages are sorted according to their name.
+     * If both pages have the same Order, they are not treated as equal.
+     * If one of the pages don't have Order set, the page that does will be sorted before the page that doesn't.
+     *
      * @author Niko Strijbol
+     *
+     * @param $a array The first page.
+     * @param $b array The second page.
+     *
+     * @return int The value to sort.
      */
-    private function sort_pages($a, $b)
+    private function sort_pages( $a, $b )
     {
-        if (empty($a['order']) && empty($b['order'])) {
-            return strcmp(basename($a['url']), basename($b['url']));
+        if (empty( $a['order'] ) && empty( $b['order'] )) {
+            return strcmp( basename( $a['url'] ), basename( $b['url'] ) );
         } else {
             if ($a['order'] == $b['order']) {
                 return 0;
-            } elseif (empty($a['order'])) {
+            } elseif (empty( $a['order'] )) {
                 return 1;
-            } elseif (empty($b['order'])) {
-                return -1;
+            } elseif (empty( $b['order'] )) {
+                return - 1;
             } else {
-                return ($a['order'] < $b['order']) ? -1 : 1;
+                return ( $a['order'] < $b['order'] ) ? - 1 : 1;
             }
         }
     }
